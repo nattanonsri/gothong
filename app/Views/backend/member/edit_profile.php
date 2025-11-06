583d45bf61f1bad070de556fa20ad3a33dd485c6
fluke <153574709+nattanonsri@users.noreply.github.com>
2025-11-07 00:16:16 +0700

    update edit_profile

--- app/Views/backend/member/edit_profile.php
+++ app/Views/backend/member/edit_profile.php
@@ -153,22 +153,6 @@
                                 <input type="date" class="form-control" id="birth_date" name="birth_date"
                                     value="<?= $user['birth_date'] ?? '' ?>" required>
                             </div>
-
-                            <!-- <div class="col-12 mt-4"></div>
-                                <h6 class="text-primary mb-3">
-                                    <i class="fa-solid fa-circle-check me-2"></i>
-                                    องค์กร
-                                </h6> -->
-                                <!-- <div class="form-check">
-                                    <select class="form-select" id="organization_id" name="organization_id" required>
-                                        <option value="">เลือกองค์กร</option>
-                                        <?php foreach ($organizations as $organization): ?>
-                                            <option value="<?= $organization['id'] ?>" <?= ($user['organization_id'] == $organization['id']) ? 'selected' : '' ?>><?= $organization['name'] ?></option>
-                                        <?php endforeach; ?>
-                                    </select>
-                                </div> -->
-                            <!-- </div> -->
-
                             <div class="col-12 text-end mt-4">
                                 <button type="button" class="btn btn-secondary me-2" onclick="window.location.href='<?= base_url('backend/profile') ?>'">
                                     <i class="fa-solid fa-times me-2"></i>
